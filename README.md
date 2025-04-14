3fc19c12026513b137a881ff3ffeea2808b607e9
jesusgr <jesusgr9511@gmail.com>
2025-04-14 16:42:34 -0600

    Se corrigen errores en los archivos para el buen funcionamiento del sitio

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Ejercicio-de-Productos
 Una aplicación web que muestra 20 productos de comercio electrónico en tarjetas , llamadas desde una api.
 
-[![proctos.png](https://i.postimg.cc/GpFJ4jBD/proctos.png)](https://postimg.cc/Y4SmVYz2)
+[![Prod.png](https://i.postimg.cc/bwk0gtvQ/Prod.png)](https://postimg.cc/QFNWxV1t)
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const main = document.getElementsByTagName("main").item(0);
+let mainProds = document.getElementById("mainProds");
+const ulMenu = document.getElementById("ulMenu")
 const URLMain = "https://fakestoreapi.com/products/";
 
-function getData(){
-    fetch(URLMain)
+function getData(cat){
+    const options ={"method":"GET"};
+    fetch(URLMain+cat)
     .then((response) => {
         console.log(response);
         response.json().then((res)=>{
@@ -19,31 +22,57 @@ function getData(){
         });
 }//getData
 
-getData();
+function getCategories(){
+    const options ={"method":"GET"};
+    fetch(URLMain+"categories/", options)
+    .then((response) => {
+        response.json().then((res)=>{
+         res.forEach((cat)=>{
+            ulMenu.insertAdjacentHTML("afterbegin",
+        `<li><a class="dropdown-item" style ="cursor:pointer;" onclick="getData('category/${(cat.replace("'","%27"))}');">${cat}</a></li>`);  
+         });
+        
+        });
+    }).catch((err)=>{
+            main.insertAdjacentHTML("beforeend",
+                `<div class="alert alert-danger" role="alert">
+                ${err.message}
+                </div>`); 
+        });
+}//getCategories
+    getCategories();
+    getData("");
+
 
 //
 
 function createCards(productos) {
+    mainProds.innerHTML="";
     const container = document.createElement("div");
-    container.className =  "row  cols 4";
-    main.insertAdjacentElement("beforeend", container);
+    container.className = "row row-cols-1 row-cols-sm-2 row-cols-md-4";
+    mainProds.appendChild(container);
 
     productos.forEach(product => {
         const card = `
         <div class="col">
             <div class="card h-100" style="width: 18rem;">
-            <img src="${product.image}" class="card-img-top" alt="${product.title}" style="height: 150px; object-fit: contain;">
+            <img src="${product.image}" class="card-img-top" alt="${product.title}" style="height: 200px; object-fit: contain;">
             <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
-            <p class="card-text">${product.description}</p>
+            <p class="card-text fw-bold">${product.category}</p>
+            <p class="card-text">${product.description.substring(0, 150)}</p>
             <p class="card-text"><strong>$${product.price}</strong></p>
-            <a href="#" class="btn btn-primary">Ver detalles</a>
+            <p class="card-text">${product.rating.rate}</p>
+          
+
+            <a href="#" class="btn btn-primary d-block mx-auto">Ver detalles</a>
             </div>
-            </div>
+        </div>
         </div>`;
         
         container.insertAdjacentHTML('beforeend', card);
     });
 }
 
-        
+       
+
